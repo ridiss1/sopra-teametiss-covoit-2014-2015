25c89aa5d8fe99c793d55414b9d0f611cc42da53
yann.mb94@gmail.com <yann.mb94@gmail.com@d28dc82d-f6d6-8d83-84cc-308bc5861b60>
2014-12-18 10:03:31 +0000

    1ere Vers

--- trunk/SopraCov/src/java/FormPage/FormServlet.java
+++ trunk/SopraCov/src/java/FormPage/FormServlet.java
@@ -7,6 +7,8 @@ package FormPage;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
+import java.sql.Time;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -33,8 +35,19 @@ public class FormServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             String name = request.getParameter("Nom");
-           String Prenom = request.getParameter("Prenom");
-           String Email = request.getParameter("Email");
+            String Prenom = request.getParameter("Prenom");
+            String Email = request.getParameter("Email");
+            int tel = Integer.parseInt(request.getParameter("Tel"));
+            String Com = request.getParameter("Commune");
+            int CodP = Integer.parseInt(request.getParameter("CodePostal"));
+            String Lieu = request.getParameter("LieuDeTravail");
+            Time Morn = Time.valueOf(request.getParameter("MorningTime"));
+            Time Eve=Time.valueOf(request.getParameter("EveTime"));
+            Date Dat = Date.valueOf(request.getParameter("DateAppli"));
+            boolean Conduc = Boolean.valueOf(request.getParameter("Conducteur"));
+            boolean Notif = Boolean.valueOf(request.getParameter("Notify"));
+           
+           
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
@@ -42,10 +55,10 @@ public class FormServlet extends HttpServlet {
             out.println("<title>Servlet FormServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            String result = data.AjoutDB(name, Prenom, Email);
+            String result = data.AjoutDB(name, Prenom, Email,tel,Com,CodP,Lieu,Morn,Eve,Dat,Conduc,Notif);
             out.println("<h1>Vous avez saisi " +result+  "</h1>");
-            out.println("<h2>The current available data within the database is:</br>"
-                    + data.selectData() + "</h2>");
+           // out.println("<h2>The current available data within the database is:</br>"
+             //       + data.selectData() + "</h2>");
 
             out.println("</body>");
             out.println("</html>");
