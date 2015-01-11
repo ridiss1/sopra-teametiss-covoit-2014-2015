ebe3ad2037a2cc0055f813173ccf2693b8b29cec
idrisslaouali <idrisslaouali@gmail.com>
2015-01-11 11:52:31 +0000

    A lire: Pour lancer l'application selon le model MVC: http://localhost:8080/SopraCov/LogServlet
    -gestion de session implement: (votre avis sur le design appliquer)
    -Modification et mise en relation des JSP et Session
    -Initialisation des cookies
    -Deconnexion
    
                                    -------------------------------------------------------------
    
     Ce pendant il ya l'adaptation pour les mobile qui n'est pas géré donc à faire je le ferai cette nuit sauf s'il quelqu'un va le faire avant: Il faut juste modifier les .JS( fichier javascript).
    Pour le design consulter les gens avant de le modifier s'il vous plait. Merci

---
+++ SopraCov/src/java/Controller/LogServlet.java
@@ -0,0 +1,178 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package Controller;
+
+import Model.DB;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.apache.derby.client.am.DateTime;
+
+/**
+ *
+ * @author UT
+ */
+@WebServlet(name = "LogServlet", urlPatterns = {"/LogServlet"})
+public class LogServlet extends HttpServlet {
+    
+    private DB data = new DB();
+    public static final String  COOKIE_DERNIERE_CONNEXION = "derniereConnexion";
+    public static final String  FORMAT_DATE               = "dd/MM/yyyy HH:mm:ss";
+    public static final String  ATT_INTERVALLE_CONNEXIONS = "intervalleConnexions";
+    public static final int     COOKIE_MAX_AGE            = 60 * 60 * 24 * 365;  // 1 an
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            
+            
+        }
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        /* Tentative de récupération du cookie depuis la requête */
+        String derniereConnexion = getCookieValue( request, COOKIE_DERNIERE_CONNEXION );
+        if(derniereConnexion!=null){
+            this.getServletContext().getRequestDispatcher("/Clientconnecter.jsp").forward(request, response);
+            
+        }else{
+            /* Affichage de la page de connexion */
+            this.getServletContext().getRequestDispatcher("/connexion.jsp").forward(request, response);
+        }
+        
+        
+        
+    }
+    
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+        /* Récupération de la session depuis la requête */
+        HttpSession session = ((HttpServletRequest) request).getSession(false);
+        session = request.getSession();
+        
+        
+        String name = request.getParameter("Nom");
+        String pass=request.getParameter("Passe");
+        if("admin".equals(name) & "admin".equals(pass))
+        {
+            RequestDispatcher rd = request.getRequestDispatcher("admin.html");
+            rd.include(request, response);
+        }
+        else
+        {
+            String S=data.verifData(name, pass);
+            if("voila".equals(S))
+                
+            {   //initialisation de la variable de session
+                
+                session.setAttribute( "Name", name );
+                this.getServletContext().getRequestDispatcher("/Clientconnecter.jsp").forward(request, response);
+                /* Récupération de la date courante */
+                Date dt = new Date();
+                /* Formatage de la date et conversion en texte */
+                java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat( FORMAT_DATE  );
+                String dateDerniereConnexion = formatter.format( dt );
+                /* Création du cookie, et ajout à la réponse HTTP */
+                setCookie( response, COOKIE_DERNIERE_CONNEXION, dateDerniereConnexion, COOKIE_MAX_AGE );
+                
+            }
+            else
+            {   session.setAttribute( "ID", null );
+            RequestDispatcher rd = request.getRequestDispatcher("nologin.html");
+            rd.include(request, response);
+            
+            }
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+    
+    
+    /**
+     * Méthode utilitaire gérant la récupération de la valeur d'un cookie donné
+     * depuis la requête HTTP.
+     */
+    private static String getCookieValue( HttpServletRequest request, String nom ) {
+        Cookie[] cookies = request.getCookies();
+        if ( cookies != null ) {
+            for ( Cookie cookie : cookies ) {
+                if ( cookie != null && nom.equals( cookie.getName() ) ) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+    
+    /*
+    * Méthode utilitaire gérant la création d'un cookie et son ajout à la
+    * réponse HTTP.
+    */
+    private static void setCookie( HttpServletResponse response, String nom, String valeur, int maxAge ) {
+        Cookie cookie = new Cookie( nom, valeur );
+        cookie.setMaxAge( maxAge );
+        response.addCookie( cookie );
+    }
+    
+}
--- SopraCov/web/Clientconnecter.jsp
+++ SopraCov/web/Clientconnecter.jsp
@@ -1,47 +1,47 @@
 <%-- 
-    Document   : result
-    Created on : 10 janv. 2015, 10:49:38
-    Author     : Jean
+    Document   : Clientconx
+    Created on : 9 janv. 2015, 22:46:50
+    Author     : Ridiss
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="fr">
+    
     <head>
-
+        
         <meta charset="utf-8">
         <title>SopraCov</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
-
+        
         <!-- CSS -->
         <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=PT+Sans:400,700'>
         <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Oleo+Script:400,700'>
         <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/style.css">
-		
-
+        
         <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
             <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
-		<!---------- CSS ------------>
-	<link rel="stylesheet" type="text/css" href="./css2/style2.css">
+        <!---------- CSS ------------>
+        <link rel="stylesheet" type="text/css" href="./css2/style2.css">
     </head>
-
+    
     <body>
         <img id="image" src="./images/Sopragroup.png" alt="" />
         <!--BEGIN #signup-form -->
-
+        
         <div class="register-container container">
             <div class="row">
+                
                 <div class="iphone span5">
                     <img src="assets/img/iphone.png" alt="">
                 </div>
                 <div class="register span6">
-                     <%-- Vérification de la présence d'un objet utilisateur en session --%>
+                    <%-- Vérification de la présence d'un objet utilisateur en session --%>
                     <c:if test="${!empty sessionScope.Name}">
                         <%-- Si l'utilisateur existe en session, alors on affiche son adresse email. --%>
                         <span> <ul id="menu">
@@ -60,40 +60,45 @@
                                     </ul></span>
                         </c:if>
                     <form method="POST" action="ProfileServlet">
-            
-                       <div>
-							<table id='results'>
-								<thead>
-									<tr id='header_row'>
-										<th>Domicile</th><th/><th/></th><th/><th/>
-										<th>Lieu de travail</th><th/><th/></th><th/><th/>
-										<th>Conducteur</th><th/><th/></th><th/><th/>
-										<th>Numero</th><th/><th/></th><th/><th/>
-                                                                                <th>Jour</th><th/><th/></th><th/><th/>
-										<th>Details</th><th/><th/></th><th/><th/>
-									</tr>
-								</thead>
-
-								<tbody id='row_1'> ${trajets} </tbody>
-
-							</table>
-						</div>
-						<p><br/>
-							<a href="Clientconnecter.jsp">Retour</a><span class="fontawesome-arrow-right"></span>
-						</p> 
-                    </form>
-
-        
+                        
+                        <!--BEGIN #subscribe-inner -->
+                        <div id="signup-inner">			
+                            <span><a class="btn-2action" href="<%=request.getContextPath()+"/ClassicServlet"%>">Recherche classique</a></span><span class="fontawesome-arrow-right"></span>
+                                
+                                <span class="btn-separator">ou </span>
+                                <span> <a class="btn-edition" href="avance.jsp">Recherche Avancee</a></span><span class="fontawesome-arrow-right"></span>
+                                
+                          
+                                <p><h3>Recherche d'itineraires</h3></p><br/><tr/> 			
+                        </div>
+                        <table>
+                            <tr>
+                            <img width="500" src="images/2.jpg" alt=""/>                               
+                            </tr>
+                        </table>
+                        
                 </div>
+                
+                
+                <!--END #signup-inner -->
             </div>
+            
+            <!--END #signup-form -->   
         </div>
+    </form>
+    
+    
+</div>
+</div>
+</div>
 
-        <!-- Javascript -->
-        <script src="assets/js/jquery-1.8.2.min.js"></script>
-        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-        <script src="assets/js/jquery.backstretch.min.js"></script>
-        <script src="assets/js/scripts.js"></script>
+<!-- Javascript -->
+<script src="assets/js/jquery-1.8.2.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/js/jquery.backstretch.min.js"></script>
+<script src="assets/js/scripts.js"></script>
 
-    </body>
+</body>
 
 </html>
+
